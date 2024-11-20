d20788dce22003035f60621c7461469e651a1911
Giga <sebagedarchia@gmail.com>
2024-11-20 18:38:17 +0400

    update models.py - add uuid to Quiz model, add verbose_name to every field

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,30 +1,32 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from user.models import User
+import uuid
 
 
 class Quiz(models.Model):
-    name = models.CharField(max_length=150)
-    creator = models.ForeignKey(User,on_delete=models.CASCADE,related_name="quizes",verbose_name=_("creator"))
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150, verbose_name=_("Name"))
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes", verbose_name=_("creator"))
 
     def __str__(self):
         return f"{self.name}"
 
 
-class Question(models.Model): 
-    question = models.TextField()
-    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE,related_name="questions")  
+class Question(models.Model):
+    question = models.TextField(verbose_name=_("Question"))
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions", verbose_name=_("Quiz"))
 
     def __str__(self):
         return f"{self.question}"
 
 
 class Answer(models.Model):
-    answer = models.TextField()
-    correct = models.BooleanField(default=False)
-    score = models.DecimalField(decimal_places=2, max_digits=5, default=1)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    answer = models.TextField(verbose_name=_("Answer"))
+    correct = models.BooleanField(default=False, verbose_name=_("Correct"))
+    score = models.DecimalField(decimal_places=2, max_digits=5, default=1, verbose_name=_("Score"))
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers", verbose_name=_("Question"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_("User"))
 
     def __str__(self):
         return f"{self.answer}"
